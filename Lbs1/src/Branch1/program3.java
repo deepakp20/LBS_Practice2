fda05d4eb3e10e08e2eb40c3b98567a2bffdad79
Deepak <Deepak@acrer>
2026-03-19 12:11:43 +0530

    initial commit

---
+++ Lbs1/src/Branch1/program3.java
@@ -0,0 +1,18 @@
+package Branch1;
+
+public class program3 {
+	public static void main(String[] args) {
+		int num = 121, reverse = 0;
+		int n = num;
+		while(num !=0) {
+			int rem = num%10;
+			reverse = reverse*10+rem;
+			num = num/10;
+		}
+		if(n == reverse) {
+			System.out.println("palindrome");
+		}else {
+			System.out.println("not");
+		}
+	}
+}
